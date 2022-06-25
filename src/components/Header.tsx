af005127df48bd1741b2cc3d7b97dd50c3a4ac40
Allison Vasconcelos <ah.vasconcelos@gmail.com>
2022-06-25 09:09:46 -0300

    conclusão aula 2

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import { Logo } from "./Logo"
 
 const Header = () => {
     return (
-        <div className="w-full h-[60px] bg-slate-800 flex justify-center align-center">
+        <div className="w-full py-5 flex justify-center align-center bg-gray-700 border-b border-gray-600">
             <Logo />
         </div>
     )
--- src/pages/Evento.tsx
+++ src/pages/Evento.tsx
@@ -1,12 +1,16 @@
 import Header from "../components/Header"
 import Video from "../components/Video"
+import SideBar from "../components/SideBar"
 
 const Evento = () => {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <Header />
-      <Video />
-    </>
+      <main className="flex flex-1">
+        <Video />
+        <SideBar />
+      </main>
+    </div>
   )
 }
 
