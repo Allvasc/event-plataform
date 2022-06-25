14f5bc74e1cd731d5a93e9e638de9f44da3c6b83
Allison Vasconcelos <ah.vasconcelos@gmail.com>
2022-06-25 16:24:14 -0300

    instalado vime e adicionados videos

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,17 +1,34 @@
-import { DiscordLogo, Lightning } from 'phosphor-react'
+import { DefaultUi, Player, Youtube } from '@vime/react'
+import { CaretRight, DiscordLogo, FileArrowDown, Image, Lightning } from 'phosphor-react'
+
+import '@vime/core/themes/default.css'
 
 const Video = () => {
     return (
         <div className='flex-1'>
             <div className='bg-black flex justify-center'>
-                <div className='h-full w-full max-w-[1100px] max-h-[60vh] aspect-video'></div>
+                <div className='h-full w-full max-w-[1100px] max-h-[60vh] aspect-video'>
+                    <Player>
+                        <Youtube videoId="cUT665tW4v8" />
+                        <DefaultUi />
+                    </Player>
+                </div>
             </div>
 
             <div className='p-8 max-w-[1100px] mx-auto'>
                 <div className='flex items-start gap-16'>
+
+
                     <div className='flex-1'>
                         <h1 className='text-2xl font-bold'>Aula - B1 - Abertura Ignite Lab</h1>
-                        <p className='mt-4 text-gray-200'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo corporis dolore tempore officia voluptate quas at eius incidunt eaque porro quaerat, tempora harum illum laboriosam ea exercitationem accusantium molestias error.</p>
+                        <p className='mt-4 text-gray-200 leading-relaxed'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo corporis dolore tempore officia voluptate quas at eius incidunt eaque porro quaerat, tempora harum illum laboriosam ea exercitationem accusantium molestias error.</p>
+                        <div className='flex items-center gap-4 mt-6'>
+                            <img className='h-16 w-16 rounded-full border-2 border-blue-500' src="https://avatars.githubusercontent.com/u/27287069?v=4" alt="imagem de perfil" />
+                            <div>
+                                <strong className='font-bold text-2sm block'>Allison Vasconcelos</strong>
+                                <span className='text-gray-200 text-sm block'>Front-end developer</span>
+                            </div>
+                        </div>
                     </div>
 
                     <div className='flex flex-col gap-4'>
@@ -24,8 +41,33 @@ const Video = () => {
                             Acesse o desafio</a>
                     </div>
                 </div>
-                <div>
 
+                <div className='gap-8 mt-20 grid grid-cols-2'>
+                    <a href="#" className='bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors'>
+                        <div className='bg-green-700 h-full p-6 flex items-center'>
+                            <FileArrowDown size={40} />
+                        </div>
+                        <div className='py-6 leading-relaxed'>
+                            <strong className='text-2xl'>Material complementar</strong>
+                            <p className='text-sm text-gray-200 mt-2'>Acesse o material complementar para acelerar o seu desenvolvimento</p>
+                        </div>
+                        <div className='h-full p-6 flex items-center'>
+                            <CaretRight size={24}/>
+                        </div>
+                    </a>
+
+                    <a href="#" className='bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors'>
+                        <div className='bg-green-700 h-full p-6 flex items-center'>
+                            <Image size={40} />
+                        </div>
+                        <div className='py-6 leading-relaxed'>
+                            <strong className='text-2xl'>Wallpapers exclusivos</strong>
+                            <p className='text-sm text-gray-200 mt-2'>Baixe wallpapers exclusivos do Ignite Lab e personalize a sua máquina</p>
+                        </div>
+                        <div className='h-full p-6 flex items-center'>
+                            <CaretRight size={24}/>
+                        </div>
+                    </a>
                 </div>
             </div>
         </div>
