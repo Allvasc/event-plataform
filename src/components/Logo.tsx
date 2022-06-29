b7f708e256356ddfd3cf8147cea06d6943aa9f03
Allison Vasconcelos <ah.vasconcelos@gmail.com>
2022-06-29 14:40:38 -0300

    criação e estilização do form

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,6 +1,6 @@
 export function Logo() {
   return (
-    <svg className="m-auto w-[200px]"
+    <svg className="w-[200px]"
       xmlns="http://www.w3.org/2000/svg"
       width="237"
       height="34"
